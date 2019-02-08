e03fc5981cfe818b6002ee05b7172d4810c80415
LightDestory <apb231@gmail.com>
2019-02-08 01:54:53 +0100

    Main.js small fix

--- utils/js/main.js
+++ utils/js/main.js
@@ -1,50 +1,49 @@
-$(document).ready( function() {
+$(document).ready(function () {
 
-	// Logo
-	var $logo 	= $('#logo');
+    // Logo
+    var $logo = $('#logo');
     var $hellologo = $('#helloworld');
-	 if (location.href.indexOf("#") != -1) {
-        if(location.href.substr(location.href.indexOf("#"))!='#about'){
-        	$logo.show();
-        }
-        else{
-            $hellologo.show();
-        }
+    if (location.href.indexOf("#") == -1 || location.href.substr(location.href.indexOf("#")) == '#about') {
+        $hellologo.show();
     }
-    
-	// Show logo 
-	$('#tab-container .tab a').click(function() {
-	  
-      $logo.slideDown('slow');
-      $hellologo.slideUp('slow');
+    else if (location.href.substr(location.href.indexOf("#")) != '#about') {
+        $logo.show();
+    }
 
-	});
-	// Hide logo
-	$('#tab-about').click(function() {
-	  $logo.slideUp('slow');
-      $hellologo.slideDown('slow');
-	});	
-function animMeter(){
-    $(".meter > span").each(function() {
-                $(this)
-                    .data("origWidth", $(this).width())
-                    .width(0)
-                    .animate({
-                        width: $(this).data("origWidth")
-                    }, 1200);
-            });
-}
-animMeter();
+    // Show logo 
+    $('#tab-container .tab a').click(function () {
 
-      $('#tab-container').easytabs({
-        animate			: true,
-        updateHash		: true,
-        transitionIn	: 'slideDown',
-        transitionOut	: 'slideUp',
-        animationSpeed	: 800,
-        tabActiveClass	: 'active'}).bind('easytabs:midTransition', function(event, $clicked, $targetPanel){
-            if($targetPanel.selector=='#resume'){
-                    animMeter();
-            }
-        });
+        $logo.slideDown('slow');
+        $hellologo.slideUp('slow');
+
     });
+    // Hide logo
+    $('#tab-about').click(function () {
+        $logo.slideUp('slow');
+        $hellologo.slideDown('slow');
+    });
+    function animMeter() {
+        $(".meter > span").each(function () {
+            $(this)
+                .data("origWidth", $(this).width())
+                .width(0)
+                .animate({
+                    width: $(this).data("origWidth")
+                }, 1200);
+        });
+    }
+    animMeter();
+
+    $('#tab-container').easytabs({
+        animate: true,
+        updateHash: true,
+        transitionIn: 'slideDown',
+        transitionOut: 'slideUp',
+        animationSpeed: 800,
+        tabActiveClass: 'active'
+    }).bind('easytabs:midTransition', function (event, $clicked, $targetPanel) {
+        if ($targetPanel.selector == '#resume') {
+            animMeter();
+        }
+    });
+});
